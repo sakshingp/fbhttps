282b25697800b7f5703b7652321209c9055b3259
sakshingp <sakshi.nagpal12@gmail.com>
2019-07-30 13:11:17 +0530

    Delete pushcrew-sw.js

--- pushcrew-sw.js
+++
@@ -1,3 +0,0 @@
-var version = 6;
-// importScripts('https://cdn.pushcrewstaging.com/sw/a86e5cec678f816f8615fb48dac667b8.js');
-importScripts('https://cdn.pushcrewstaging.com/sw/b5bdeeb5f80087c82883cfb4008f6063.js');
